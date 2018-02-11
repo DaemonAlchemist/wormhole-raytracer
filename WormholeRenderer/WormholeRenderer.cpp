e6e4bb183d0bc4cec72a6837064c5de5cc2026c9
Andrew Wittrock <andy@wittrock.us>
2018-02-10 23:35:58 -0600

    Initial setup test

--- WormholeRenderer/WormholeRenderer.cpp
+++ WormholeRenderer/WormholeRenderer.cpp
@@ -1,11 +1,15 @@
 // WormholeRenderer.cpp : Defines the entry point for the console application.
 //
 
-#include "stdafx.h"
-
+#include "vendor\daemonalchemist\atp-vector\src\vector.hpp"
+#include <iostream>
 
 int main()
 {
+	ATP::Math::Vector v(1, 2, 3);
+
+	std::cout << v.x << "\n" << v.y << "\n" << v.z;
+
     return 0;
 }
 
